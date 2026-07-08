116a802e362e27b0a9ce93dbca3ad7578b313a90
ZeoxLiu <zxlee926@gmail.com>
2026-07-08 22:06:31 +0800

    feat:day04:learning copy constructor

---
+++ code/day04/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+
+
+class File {
+public:
+    File() {
+        std::cout << "Open File\n";
+    }
+
+    ~File() {
+        std::cout << "Close File\n";
+    }
+    File(const File& other) {
+        std::cout << " copy construct\n";
+    }
+};
+
+ void process(File file) {
+        std::cout<<"processing";
+    }
+    
+int main() {
+    File file1;
+    process(file1);
+}
